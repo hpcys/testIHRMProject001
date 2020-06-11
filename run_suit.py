5ed7d5bc1ed7d36854fc0e005480291a479c2d22
hpcys2496393489@qq.com <hpcys2496393489@qq.com>
2020-06-11 10:32:24 +0800

    修改后的巡回

--- run_suit.py
+++ run_suit.py
@@ -11,3 +11,5 @@ suite = unittest.TestLoader().discover(BASE_DIR + '/script', 'test*.py')
 # file = 'ihrm_{}.html'.format(time.strftime('%Y%m%d%H%M%S'))
 file = 'ihrm.html'
 BeautifulReport(suite).report(filename=file, description='完美报告', log_path=BASE_DIR + '/report')
+print("--"*100)
+print("aaaaaaaa")
